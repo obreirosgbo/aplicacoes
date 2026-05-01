32fd44e822e385a71d64021613aaaa2775404533
Herbert Leão <herbertscont@gmail.com>
2026-05-01 20:10:50 -0300

    supabase

--- supabase.js
+++ supabase.js
@@ -4,8 +4,8 @@
 // Substitua os valores abaixo após criar seu projeto:
 // https://app.supabase.com → Settings → API
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://thhpgzxfmqnccydyqtdr.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoaHBnenhmbXFuY2N5ZHlxdGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2NTA5OTEsImV4cCI6MjA5MzIyNjk5MX0.wMDWSavFGMv0BYOF0wHvTKBQ2tQlEr5xRULQn_aG6Bg';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
